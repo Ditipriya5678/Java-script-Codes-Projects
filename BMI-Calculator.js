88687f61e56182059d4a1a2db0ff0bda855886bb
Ditipriya Seal <67802174+Ditipriya5678@users.noreply.github.com>
2021-07-26 15:17:48 +0530

    Update BMI-Calculator.js

--- BMI-Calculator.js
+++ BMI-Calculator.js
@@ -1,25 +1,28 @@
-function bmiCalculator (weight, height) {
+/*BMI Calculator*/
+
+function bmiCalculator (weight, height) 
+{
     var bmi = (weight / (height * height));
     bmi = Math.round(bmi);
     return bmi;
 }
 
-var bmi = bmiCalculator (60,2);
+var bmi = bmiCalculator (prompt("Enter weight:"),prompt("Enter height:"));
 if(bmi<18.5)
     {
-        console.log('\"Your BMI is ' + bmi + ', so you are underweight.\"');
+        alert('\"Your BMI is ' + bmi + ', so you are underweight.\"');
     }
 else 
 {
     if ( bmi >= 18.5 && bmi <= 24.9) 
     {
-       console.log('\"Your BMI is ' + bmi + ', so you have a normal weight.\"');
+       alert('\"Your BMI is ' + bmi + ', so you have a normal weight.\"');
     } 
     else 
     {
       if ( bmi > 24.9) 
       {
-        console.log('\"Your BMI is ' + bmi + ', so you are overweight.\"');
+        alert('\"Your BMI is ' + bmi + ', so you are overweight.\"');
       }
     }
 }
